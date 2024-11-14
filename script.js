70328ee079362aad48479573225cb5f7900939cf
arshad012 <forarshad99@gmail.com>
2024-11-15 00:30:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+document.querySelector('#btn').addEventListener('click', () => {
+
+	const message = document.getElementById('text').value;
+	const delay = document.getElementById('delay').value;
+
+	const displayMessage = async () => {
+		let newPromise = new Promise((resolve, reject) => {
+			return resolve(message);
+		});
+
+		
+		let result = await newPromise;
+		setTimeout(() => {
+			document.getElementById('output').innerText = result;
+		},delay)
+	};
+
+	displayMessage();
+
+	
+})
+
+
